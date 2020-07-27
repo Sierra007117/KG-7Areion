07e5e97754e0fdcce197caee4d83d415cce079ff
Mahadi Sajjad Neloy <sierra117@qq.com>
2020-07-27 16:19:35 +0800

    Update demo_clock_and_IP.py

--- demo_clock_and_IP.py
+++ demo_clock_and_IP.py
@@ -3,13 +3,14 @@ import time
 import datetime
 import subprocess
 display = lcddriver.lcd()
-IP = subprocess.check_output(["hostname","-I"]).split()[0]
+IP = subprocess.check_output(["hostname", "-I"]).split()[0]
 try:
     print("Writing to display")
     while True:
         display.lcd_display_string(str(datetime.datetime.now().time()), 1)
         display.lcd_display_string(str(IP), 2)
         # Program then loops with no delay (Can be added with a time.sleep)
-except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+# If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+except KeyboardInterrupt:
     print("Cleaning up!")
     display.lcd_clear()
